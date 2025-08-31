c7bdd79719d716e1053c5186519b45d1daa9e62a
Carlos Adolfo <adolfo.ggc@gmail.com>
2025-08-31 12:28:03 -0300

    Add useEffect and useState on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,34 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { knowledgesType } from "./api/route";
+
 export default function Home() {
+  const [apiData, setApiData] = useState <knowledgesType | null>(null)
+
+  useEffect(() =>  {
+    console.log('Fetching...');
+    fetch('http://localhost:8080/api')
+      .then(response => response.json())
+      .then(data => {
+        setApiData(data)
+        console.log(data)}
+      )
+      .catch(error => console.error('Error fetching data:', error));
+  }, [])
+  
   return(
-    <div>Home</div>
+    <>
+      <div>Home</div>
+      {
+        apiData 
+        ? 
+        apiData.map(knowledge => (
+          <div key={knowledge.name}>{knowledge.name}</div>
+        ))
+        : <div>Loading...</div>
+      }
+    </>
+    
   )
 }
