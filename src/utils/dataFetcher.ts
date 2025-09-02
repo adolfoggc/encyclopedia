a239d91cab5d6887ea9a441cf62801722a70701a
Carlos Adolfo <adolfo.ggc@gmail.com>
2025-09-02 16:24:02 -0300

    Add postFetcher

--- src/utils/dataFetcher.ts
+++ src/utils/dataFetcher.ts
@@ -1,4 +1,14 @@
 
-export function dataFetcher(url: string): Promise<any> {
+export function getFetcher(url: string): Promise<any> {
   return fetch(url).then(response => response.json())
+}
+
+export function postFetcher(url: string, body: any): Promise<any> {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }).then(response => response.json())
 }
