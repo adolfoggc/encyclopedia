2e6f23be3591a10f4a38ef4129b2498189464906
Carlos Adolfo <adolfo.ggc@gmail.com>
2025-09-02 15:34:49 -0300

    Create type for API

---
+++ src/app/api/types.ts
@@ -0,0 +1,14 @@
+export type knowledgeType = {
+  name: string,
+  topics: topicType[]
+}
+
+export type topicType = {
+  name: string,
+  tips: tipType[]
+}
+
+export type tipType = {
+  text: string,
+  related?: string[]
+}
