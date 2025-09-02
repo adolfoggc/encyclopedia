b12543dc61c9a5fb5197c08330b38fdd7e5c38b5
Carlos Adolfo <adolfo.ggc@gmail.com>
2025-09-02 16:25:31 -0300

    Add mock post response

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,3 +4,7 @@ import { knowledges } from './knowledges';
 export async function GET() {
   return NextResponse.json( knowledges );
 }
+
+export async function POST() {
+  return NextResponse.json( { message: 'POST request received' } );
+}
