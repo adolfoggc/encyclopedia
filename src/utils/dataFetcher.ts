9a56eff9dc6e5446e3a05f5487523caa7b6ab52c
Carlos Adolfo <adolfo.ggc@gmail.com>
2025-09-03 18:09:18 -0300

    Add types to datafetcher

--- src/utils/dataFetcher.ts
+++ src/utils/dataFetcher.ts
@@ -1,9 +1,10 @@
+import { knowledgeType, requestBodyType, topicType } from "@/app/api/types"
 
-export function getFetcher(url: string): Promise<any> {
+export function getFetcher(url: string): Promise<knowledgeType[]> {
   return fetch(url).then(response => response.json())
 }
 
-export function postFetcher(url: string, body: any): Promise<any> {
+export function postFetcher(url: string, body: requestBodyType): Promise<topicType[] | { error: string }> {
   return fetch(url, {
     method: 'POST',
     headers: {
