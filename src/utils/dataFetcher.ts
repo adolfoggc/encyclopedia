a9a115adc772a1b12e015055671ec9f7873482e2
Carlos Adolfo <adolfo.ggc@gmail.com>
2025-09-08 17:28:42 -0300

    Update dataFetcher

--- src/utils/dataFetcher.ts
+++ src/utils/dataFetcher.ts
@@ -1,10 +1,10 @@
-import { knowledgeType, requestBodyType, topicType } from "@/app/api/types"
+import { knowledgeType, requestBodyType, tipType, topicType } from "@/app/api/types"
 
 export function getFetcher(url: string): Promise<knowledgeType[]> {
   return fetch(url).then(response => response.json())
 }
 
-export function postFetcher(url: string, body: requestBodyType): Promise<topicType[] | { error: string }> {
+export function postFetcher(url: string, body: requestBodyType): Promise<tipType[]> {
   return fetch(url, {
     method: 'POST',
     headers: {
